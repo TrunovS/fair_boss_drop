ce16a836bd143496c02e1461b8c3fdb4d1aba9ac
Trunov Sergey <trunovsergey@list.ru>
2019-01-08 18:25:07 +0300

    Подготовил топовой хендлер.

---
+++ src/handlers.rs
@@ -0,0 +1,34 @@
+use iron::*;
+use iron::mime::{Mime, TopLevel, SubLevel};
+use postgres::Connection;
+
+use std::io::Read;
+use std::sync::Mutex;
+
+pub fn get_bosses(sdb: &Mutex<PostgresSqlData>, req: &mut Request) -> IronResult<Response> {
+    let url = req.url.clone().into_generic_url();
+    let path = url.path().unwrap();
+    let sid: &str = &path.iter().last().unwrap();
+    let id;
+    if let Ok(r) = sid.parse() {
+        id = r;
+    } else {
+        return Ok(Response::with((status::BadRequest, "bad id")));
+    }
+
+    let json_record;
+    if let Ok(recs) = ::db::read_one(sdb, id) {
+        if let Ok(json) = json::encode(&recs) {
+            json_record = Some(json);
+        } else {
+            return Ok(Response::with((status::InternalServerError,
+                                      "couldn't convert records to JSON")));
+        }
+    } else {
+        return Ok(Response::with((status::InternalServerError,
+                                  "couldn't read records from database")));
+    }
+    let content_type = Mime(TopLevel::Application, SubLevel::Json, Vec::new());
+
+    Ok(Response::with((content_type, status::Ok, json_record.unwrap())))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+extern crate iron;
+extern crate router;
+extern crate url;
+
+use iron::*;
+
 mod BdLayer;
+mod handlers;
+
 use BdLayer::PostgresDealer::*;
 use BdLayer::PostgresCommands::*;
+use std::sync::{Arc, Mutex};
+
+fn serve(db: PostgresSqlData) {
+    let sdb = Arc::new(Mutex::new(db));
+    let mut router = router::Router::new();
+    {
+        let sdb_ = sdb.clone();
+        router.get("/v0/bosses",
+                   move |req: &mut Request|
+                   handlers::get_bosses(sdb_.clone(), req));
+    }
+    Iron::new(router).http("localhost:3000").unwrap();
+}
 
 
 fn main() {
@@ -8,7 +29,7 @@ fn main() {
     bd_data.connect().unwrap();
     bd_data.doCommand(PostgresInitTables::new()).unwrap();
     println!("status of connection: {}", bd_data.isOpen());
-    bd_data.finish();
+    bd_data.finish().unwrap();
     println!("status of connection: {}", bd_data.isOpen());
 
     println!("Hello, world!");
