b4bff8e2605cf3b65099d6a84f677fd3ad157f90
tserg <trunovsergey@list.ru>
2019-01-09 19:13:40 +0300

    Команда инициализации таблиц.

--- src/BdLayer/PostgresCommands.rs
+++ src/BdLayer/PostgresCommands.rs
@@ -1,5 +1,3 @@
-// pub mod PostgresDealer;
-
 use BdLayer::PostgresDealer::postgres::{Connection, error::Error};
 
 pub trait PostgresCommand {
@@ -15,14 +13,36 @@ impl PostgresInitTables {
 
 impl PostgresCommand for PostgresInitTables {
     fn execute(&self,connect: &Connection) -> Result<(),Error> {
-        connect.batch_execute("
-            CREATE TABLE subperson (
+        let trans = connect.transaction().unwrap();
+        let res = trans.batch_execute("
+            CREATE TABLE IF NOT EXISTS item_types (
             id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL );
+            label VARCHAR NOT NULL
+            );
 
-            CREATE TABLE person (
+            CREATE TABLE IF NOT EXISTS items (
             id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL );
-           ")
+            label VARCHAR NOT NULL,
+            type SERIAL REFERENCES item_types (id)
+            );
+
+DO $$ BEGIN
+            CREATE TYPE item_probability AS (
+            id INTEGER,
+            probability NUMERIC(7,6)
+            );
+            EXCEPTION
+               WHEN duplicate_object THEN null;
+END $$;
+
+            CREATE TABLE IF NOT EXISTS bosses (
+            id SERIAL PRIMARY KEY,
+            label VARCHAR NOT NULL,
+            level INTEGER NOT NULL DEFAULT '0',
+            drop item_probability[]
+            );
+           ");
+        trans.commit().unwrap();
+        res
     }
 }
