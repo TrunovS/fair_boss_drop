4d35e5b533f3ca10544d57ef82ae887cb5b9f4fa
tserg <trunovsergey@list.ru>
2019-01-09 16:19:42 +0300

    Запуск сервера с запуском пробной комманды.

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,36 +5,38 @@ use postgres::Connection;
 use std::io::Read;
 use std::sync::Mutex;
 
-mod BdLayer;
 use BdLayer::PostgresCommands::*;
 use BdLayer::PostgresDealer::*;
 
 pub fn get_bosses(sdb: &Mutex<PostgresSqlData>, req: &mut Request) -> IronResult<Response> {
-    // bd_data.doCommand(PostgresInitTables::new()).unwrap();
+    let mut bd_data = sdb.lock().unwrap();
+    bd_data.doCommand(PostgresInitTables::new()).unwrap();
 
-    let url = req.url.clone().into_generic_url();
-    let path = url.path().unwrap();
-    let sid: &str = &path.iter().last().unwrap();
-    let id;
-    if let Ok(r) = sid.parse() {
-        id = r;
-    } else {
-        return Ok(Response::with((status::BadRequest, "bad id")));
-    }
+    Ok(Response::with((status::Ok,"Command executed")))
 
-    let json_record;
-    if let Ok(recs) = ::db::read_one(sdb, id) {
-        if let Ok(json) = json::encode(&recs) {
-            json_record = Some(json);
-        } else {
-            return Ok(Response::with((status::InternalServerError,
-                                      "couldn't convert records to JSON")));
-        }
-    } else {
-        return Ok(Response::with((status::InternalServerError,
-                                  "couldn't read records from database")));
-    }
-    let content_type = Mime(TopLevel::Application, SubLevel::Json, Vec::new());
+    // let url = req.url.clone().into_generic_url();
+    // let path = url.path().unwrap();
+    // let sid: &str = &path.iter().last().unwrap();
+    // let id;
+    // if let Ok(r) = sid.parse() {
+    //     id = r;
+    // } else {
+    //     return Ok(Response::with((status::BadRequest, "bad id")));
+    // }
 
-    Ok(Response::with((content_type, status::Ok, json_record.unwrap())))
+    // let json_record;
+    // if let Ok(recs) = ::db::read_one(sdb, id) {
+    //     if let Ok(json) = json::encode(&recs) {
+    //         json_record = Some(json);
+    //     } else {
+    //         return Ok(Response::with((status::InternalServerError,
+    //                                   "couldn't convert records to JSON")));
+    //     }
+    // } else {
+    //     return Ok(Response::with((status::InternalServerError,
+    //                               "couldn't read records from database")));
+    // }
+    // let content_type = Mime(TopLevel::Application, SubLevel::Json, Vec::new());
+
+    // Ok(Response::with((content_type, status::Ok, json_record.unwrap())))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
 extern crate iron;
 extern crate router;
 extern crate url;
@@ -14,9 +20,10 @@ fn serve(db: PostgresSqlData) {
     let mut router = router::Router::new();
     {
         let sdb_ = sdb.clone();
-        router.get("/v0/bosses",
+        router.get("/api/v0/bosses",
                    move |req: &mut Request|
-                   handlers::get_bosses(sdb_.clone(), req));
+                   handlers::get_bosses(&sdb_.clone(), req),
+                   "test_bosses");
     }
     Iron::new(router).http("localhost:3000").unwrap();
 }
@@ -25,6 +32,7 @@ fn serve(db: PostgresSqlData) {
 fn main() {
     let mut bd_data = PostgresSqlData::new();
     bd_data.connect().unwrap();
+    serve(bd_data);
 
     println!("Hello, world!");
 }
